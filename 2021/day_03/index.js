8c48966631850b8c3e712f80d2b4c9a4ab9246de
Finn Kumkar <finn.kumkar@gmail.com>
2021-12-10 23:22:07 +0100

    Add solution for day three part two

--- 2021/day_03/index.js
+++ 2021/day_03/index.js
@@ -54,8 +54,75 @@ const getGammaRate = diagnostics => {
  */
 const getEpsilonRate = diagnostics => getGammaRate(diagnostics).map(bit => 1 - bit);
 
+/**
+ * Counts the high and low bits at a specific position.
+ *
+ * @param {Array<string>} diagnostics The binary output of the diagnostics report.
+ * @param {number} position The bit position in the binary number.
+ * @returns {Array<number>} The count result.
+ */
+const countBits = (diagnostics, position) => {
+
+    const count = [0,0];
+
+    diagnostics.forEach(entry => {
+
+        const bit = parseInt(entry.charAt(position), 2);
+
+        if (isNaN(bit)) {
+            return;
+        }
+
+        count[bit]++;
+    });
+
+    return count;
+}
+
+/**
+ * Filters and reduces the entries by high or low bit priority.
+ *
+ * @param {Array<string>} diagnostics The binary output of the diagnostics report.
+ * @param {number} priority The bit priority.
+ * @returns {number} The decimal value of the final entry.
+ */
+const filterBits = (diagnostics, priority) => {
+
+    const max = diagnostics[0].length;
+
+    let items = diagnostics;
+
+    for (let position = 0; position < max ; position++) {
+
+        let count = countBits(items, position);
+
+        let filterBit;
+
+        if (priority === 1) {
+            filterBit = count[1] >= count[0] ? 1 : 0;
+        } else {
+            filterBit = count[0] <= count[1] ? 0 : 1;
+        }
+
+        items = items.filter(entry => parseInt(entry.charAt(position), 2) === filterBit);
+
+        if (items.length === 1) {
+            break;
+        }
+    }
+
+    return parseInt(items[0], 2);
+}
+
 console.log(
     'Solution for part one:',
     parseInt(getGammaRate(readInput()).join(''), 2) *
     parseInt(getEpsilonRate(readInput()).join(''), 2)
 );
+
+console.log(
+    'Solution for part two:',
+    filterBits(readInput(), 0) *
+    filterBits(readInput(), 1)
+);
+
